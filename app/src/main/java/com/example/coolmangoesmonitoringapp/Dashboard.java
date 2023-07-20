28bf36332a5dba1dbe76cc6b744412d0aa361ce4
faridibtidatashbeeh <tashbeiph@gmail.com>
2023-07-21 00:20:17 +0100

    Added onclick for button for both temp and humidity graph

--- app/src/main/java/com/example/coolmangoesmonitoringapp/Dashboard.java
+++ app/src/main/java/com/example/coolmangoesmonitoringapp/Dashboard.java
@@ -2,7 +2,10 @@ package com.example.coolmangoesmonitoringapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Dashboard extends AppCompatActivity {
 
@@ -10,5 +13,24 @@ public class Dashboard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        Button temperature_button = (Button)findViewById(R.id.tempBtn);
+        Button humidity_btn = (Button)findViewById(R.id.humBtn);
+
+        temperature_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Dashboard.this, TemperatureInfo.class);
+                startActivity(intent);
+            }
+        });
+
+        humidity_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Dashboard.this, HumidityInfo.class);
+                startActivity(intent);
+            }
+        });
     }
 }
